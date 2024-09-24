aa9edf0ea41d4677c355562ca70eae6d17d48891
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-09-24 15:16:10 -0300

    feat: criação do classe model

---
+++ backend/src/main/java/com/vinicius/locadora/model/Classe.java
@@ -0,0 +1,26 @@
+package com.vinicius.locadora.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Classe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private double valor;
+    private Date prazoDevolucao; 
+}
