900ee470724ec6b2e99ce9063bee4242e794f78b
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-09-24 15:16:52 -0300

    feat: criação do ator repository

---
+++ backend/src/main/java/com/vinicius/locadora/repository/AtorRepository.java
@@ -0,0 +1,9 @@
+package com.vinicius.locadora.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vinicius.locadora.model.Ator;
+
+public interface AtorRepository extends JpaRepository<Ator, Integer> {
+    
+}
