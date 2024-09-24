dd6087fc9f8f37c8cc9647e25238fb5385365a8b
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-09-24 11:06:47 -0300

    feat: página de classe

---
+++ frontend/src/app/pages/classe/classe.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LayoutBaseComponent } from '../../components/layout-base/layout-base.component';
+
+@Component({
+  selector: 'app-classe',
+  standalone: true,
+  imports: [LayoutBaseComponent],
+  templateUrl: './classe.component.html',
+  styles: ``
+})
+export class ClasseComponent {
+
+}
