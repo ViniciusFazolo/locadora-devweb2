4d6d085b51f47d4250361327e3cba4ee9c48341b
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-09-24 10:50:38 -0300

    ci: rota de ator

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AtorComponent } from './pages/ator/ator.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
+        component: HomeComponent,
+    },
+    {
+        path: 'ator',
+        component: AtorComponent
     }
 ];
