cd13dd699702388e042bfe7f721a12fc84680f86
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-09-24 11:08:05 -0300

    ci: adicionado rota de classe

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AtorComponent } from './pages/ator/ator.component';
+import { ClasseComponent } from './pages/classe/classe.component';
+import { DiretorComponent } from './pages/diretor/diretor.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +12,10 @@ export const routes: Routes = [
     {
         path: 'ator',
         component: AtorComponent
-    }
+    },
+    {
+        path: 'classe',
+        component: ClasseComponent
+    },
+   
 ];
