79b6091f7f4b785a889f7cee0ebf21f9b1adae71
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-09-26 10:01:04 -0300

    feat: repository do diretor

---
+++ backend/src/main/java/com/vinicius/locadora/repository/DiretorRepository.java
@@ -0,0 +1,7 @@
+package com.vinicius.locadora.repository;
+
+import com.vinicius.locadora.model.Diretor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiretorRepository extends JpaRepository<Diretor, Integer> {
+}
