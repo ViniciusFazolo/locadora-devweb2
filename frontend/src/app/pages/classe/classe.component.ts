f5de92c9837be6a4b43af62ba157791d3da7f95e
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-09-30 14:05:48 -0300

    feat: create classe component

--- frontend/src/app/pages/classe/classe.component.ts
+++ frontend/src/app/pages/classe/classe.component.ts
@@ -1,13 +1,145 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LayoutBaseComponent } from '../../components/layout-base/layout-base.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { DialogComponent } from '../../components/dialog/dialog.component';
+import { ClasseService } from '../../services/classe.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Classe } from '../../interfaces/classe';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-classe',
   standalone: true,
-  imports: [LayoutBaseComponent],
+  imports: [LayoutBaseComponent, TableModule, DialogComponent, ButtonModule, ToastModule, SplitButtonModule, ConfirmPopupModule, FormsModule, CalendarModule],
   templateUrl: './classe.component.html',
+  providers: [MessageService, ConfirmationService],
   styles: ``
 })
-export class ClasseComponent {
+export class ClasseComponent implements OnInit{
+  isDialogOpen: boolean = false;
+  items!: Classe[]
+  itemToEdit!: Classe | null;
+  classe: string = '';
+  prazoDevolucao!: Date;
+  valor: number = 0;
+
+  constructor(private messageService: MessageService, private classeService: ClasseService, private confirmationService: ConfirmationService) {}
+
+  ngOnInit(): void {
+    this.listAll()
+  }
+
+  toggleDialog(){
+    this.classe = ''
+    this.prazoDevolucao = new Date()
+    this.valor = 0
+    this.isDialogOpen = !this.isDialogOpen
+  }
+
+  listAll(){
+    this.classeService.listAll().subscribe({
+      next: (res) => {
+        this.items = res.dados
+      },
+      error: () => {
+        this.messageService.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao listar classes' });
+      }
+    })
+  }
+
+  handleSave(){
+    if(!this.classe){
+      this.messageService.add({ severity: 'warn', summary: 'Aviso', detail: 'Preencha todos os campos' });
+      return
+    }
+
+    if(this.itemToEdit){
+      this.edit()
+    }else{
+      this.create()
+    }
+  }
+
+  handleEdit(id: number){
+    this.classeService.listById(id).subscribe({
+      next: (res) => {
+        this.itemToEdit = res.dados
+        this.classe = this.itemToEdit.nome;
+        this.isDialogOpen = true
+      },
+      error: () => {
+        this.messageService.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao editar classe' });
+      }
+    })
+  }
+
+  handleDelete(event: Event, id: number){
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Realmente deseja excluir este registro?',
+      icon: 'pi pi-info-circle',
+      acceptButtonStyleClass: 'p-button-danger p-button-sm',
+      accept: () => {
+        this.delete(id)
+      },
+    });
+  }
+
+  edit(){
+    const obj: Classe = {
+      nome: this.classe,
+      prazoDevolucao: this.prazoDevolucao,
+      valor: this.valor,
+      id: this.itemToEdit?.id
+    }
+
+    this.classeService.update(obj).subscribe({
+      next: () => {
+        this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Registro atualizado com sucesso', life: 3000 });
+        this.itemToEdit = null
+        this.listAll()
+        this.toggleDialog()
+      },
+      error: () => {
+        this.messageService.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao atualizar registro' });
+      }
+    })
+  }
+
+  create(){
+    const obj: Classe = {
+      nome: this.classe,
+      prazoDevolucao: this.prazoDevolucao,
+      valor: this.valor,
+    }
+
+    this.classeService.create(obj).subscribe({
+      next: () => {
+        this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Registro inserido com sucesso', life: 3000 });
+        this.listAll()
+        this.toggleDialog()
+      },
+      error: () => {
+        this.messageService.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao inserir registro' });
+      }
+    })
+  }
+
+  delete(id: number){
+    this.classeService.delete(id).subscribe({
+      next: () => {
+        this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Registro excluído com sucesso', life: 3000 });
+        this.listAll()
+      },
+      error: () => {
+        this.messageService.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao excluir registro' });
+      }
+    })
+  }
 
 }
