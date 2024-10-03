8f8b7685c98e5cf96dbc107ac348ee966da4974c
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-10-03 10:30:59 -0300

    cleanup: importações não usadas

--- backend/src/main/java/com/vinicius/locadora/controller/ClasseController.java
+++ backend/src/main/java/com/vinicius/locadora/controller/ClasseController.java
@@ -6,7 +6,6 @@ import com.vinicius.locadora.DTO.RequestDTO.ClasseRequestDTO;
 import com.vinicius.locadora.DTO.ResponseDTO.ClasseResponseDTO;
 import com.vinicius.locadora.service.ClasseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
