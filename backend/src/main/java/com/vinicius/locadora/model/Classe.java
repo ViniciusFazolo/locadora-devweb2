d46b9677b93e685386203b66c929a1d6abc9e822
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-10-24 22:34:16 -0300

    feat: relacionamento com titulo

--- backend/src/main/java/com/vinicius/locadora/model/Classe.java
+++ backend/src/main/java/com/vinicius/locadora/model/Classe.java
@@ -1,11 +1,13 @@
 package com.vinicius.locadora.model;
 
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,4 +25,7 @@ public class Classe {
     private String nome;
     private Double valor;
     private Date prazoDevolucao; 
+
+    @OneToMany(mappedBy = "classe")
+    private List<Titulo> titulos;
 }
