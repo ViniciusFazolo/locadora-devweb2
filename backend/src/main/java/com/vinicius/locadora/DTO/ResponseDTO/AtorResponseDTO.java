45dc07f61ce27038887e82cc137833ba5faf4d9f
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-10-26 14:26:38 -0300

    refactor: removido titulo do dto, pq essa lista quem cuida é a tabela intermediaria

--- backend/src/main/java/com/vinicius/locadora/DTO/ResponseDTO/AtorResponseDTO.java
+++ backend/src/main/java/com/vinicius/locadora/DTO/ResponseDTO/AtorResponseDTO.java
@@ -1,13 +1,8 @@
 package com.vinicius.locadora.DTO.ResponseDTO;
 
-import java.util.List;
-
-import com.vinicius.locadora.model.Titulo;
-
 public record AtorResponseDTO(
     int id,
-    String nome,
-    List<Titulo> titulos
+    String nome
 ) {
     
 }
