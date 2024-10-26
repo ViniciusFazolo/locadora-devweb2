bc23dee7ad6f8e0960d02c2b94721b294867fed8
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-10-26 14:26:07 -0300

    feat: adicionado titulos no dto

--- backend/src/main/java/com/vinicius/locadora/DTO/RequestDTO/ClasseRequestDTO.java
+++ backend/src/main/java/com/vinicius/locadora/DTO/RequestDTO/ClasseRequestDTO.java
@@ -1,11 +1,15 @@
 package com.vinicius.locadora.DTO.RequestDTO;
 
 import java.util.Date;
+import java.util.List;
+
+import com.vinicius.locadora.model.Titulo;
 
 public record ClasseRequestDTO(
     String nome,
     Double valor,
-    Date prazoDevolucao
+    Date prazoDevolucao,
+    List<Titulo> titulos
 ) {
     
 }
