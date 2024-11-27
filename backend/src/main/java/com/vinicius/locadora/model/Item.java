9761d283b27121067e7ac7f1abbe2b6bd2cc7ee8
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-11-26 21:43:02 -0300

    feat: lista de locacao

--- backend/src/main/java/com/vinicius/locadora/model/Item.java
+++ backend/src/main/java/com/vinicius/locadora/model/Item.java
@@ -1,12 +1,14 @@
 package com.vinicius.locadora.model;
 
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,4 +30,7 @@ public class Item {
 
     @ManyToOne
     private Titulo titulo;
+
+    @OneToMany(mappedBy = "item")
+    private List<Locacao> locacao;
 }
