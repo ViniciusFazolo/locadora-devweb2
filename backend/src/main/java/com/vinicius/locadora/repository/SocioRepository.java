99d876d723ff97cd44f1a9b9d03045d2795b981f
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-11-27 13:28:10 -0300

    feat: repository de sócio

---
+++ backend/src/main/java/com/vinicius/locadora/repository/SocioRepository.java
@@ -0,0 +1,9 @@
+package com.vinicius.locadora.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vinicius.locadora.model.Socio;
+
+public interface SocioRepository extends JpaRepository<Socio, Integer> {
+    
+}
