bd780b0a1a19d535dcb278cdf3d7695d55d5c50d
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-11-26 22:06:53 -0300

    feat: controller de locação

---
+++ backend/src/main/java/com/vinicius/locadora/controller/LocacaoController.java
@@ -0,0 +1,51 @@
+package com.vinicius.locadora.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vinicius.locadora.DTO.RequestDTO.LocacaoRequestDTO;
+import com.vinicius.locadora.DTO.ResponseDTO.LocacaoResponseDTO;
+import com.vinicius.locadora.service.LocacaoService;
+
+@RestController
+@RequestMapping("/locacao")
+public class LocacaoController {
+    
+    @Autowired
+    private LocacaoService locacaoService;
+
+    @PostMapping("/novo")
+    public ResponseEntity<LocacaoResponseDTO> salvar(@RequestBody LocacaoRequestDTO request){
+        return locacaoService.salvar(request);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LocacaoResponseDTO> buscarPorId(@PathVariable int id){
+        return locacaoService.buscarPorId(id);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<LocacaoResponseDTO>> buscarTodos(){
+        return locacaoService.buscarTodos();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LocacaoResponseDTO> atualizar(@RequestBody LocacaoRequestDTO request){
+        return locacaoService.atualizar(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletar(@PathVariable int id){
+        return locacaoService.deletar(id);
+    }
+}
