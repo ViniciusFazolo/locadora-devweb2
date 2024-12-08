427e2a7e3eb5bb0eae087ec528473347579be627
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2024-12-08 12:14:18 -0300

    cleanup: import nao usado

--- backend/src/main/java/com/vinicius/locadora/model/Socio.java
+++ backend/src/main/java/com/vinicius/locadora/model/Socio.java
@@ -3,7 +3,6 @@ package com.vinicius.locadora.model;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
